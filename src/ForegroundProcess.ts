c9baf54dac78f5add275ec0ae7369850705d4a96
Will <will.streeter@gmail.com>
2016-10-25 22:08:49 -0700

    udpates..

---
+++ src/ForegroundProcess.ts
@@ -0,0 +1,3 @@
+import * as _ from "lodash";
+import { ipcRenderer } from "electron";
+import { uuid } from "uuid";
--- src/MainProcess.ts
+++ src/MainProcess.ts
@@ -1,2 +1,52 @@
 import * as _ from "lodash";
-import * as electron from "electron";
+import {BrowserWindow, ipcMain} from "electron";
+
+
+
+export class MediaProcess{
+
+    public foregroundWindows:any = [];
+
+    public backgroundProcessHandler:any = {
+        addWindow(browserWindow) {
+            this.foregroundWindows.push(browserWindow);
+        }
+    }
+
+    constructor(){}
+
+    createBackgroundProcess(url:string, debug:any) {
+        const backgroundWindow = new BrowserWindow();
+
+        if (!debug) {
+            backgroundWindow.hide();
+        }
+
+        backgroundWindow.loadURL(url);
+
+        ipcMain.on('BACKGROUND_START', (event, result) => {
+            backgroundWindow.webContents.send.apply(backgroundWindow.webContents, ['BACKGROUND_START', result]);
+        });
+
+
+        ipcMain.on('BACKGROUND_REPLY', (event, result) => {
+            this.sendToAllForegroundWindows('BACKGROUND_REPLY', result);
+        });
+
+
+        ipcMain.on('CALLBACK', (event, payload) => {
+            this.sendToAllForegroundWindows('CALLBACK', payload);
+        });
+
+
+        return this.backgroundProcessHandler;
+    }
+
+    private sendToAllForegroundWindows(eventName:string, payload:any) {
+        _.forEach(this.foregroundWindows, (foregroundWindow) => {
+            foregroundWindow.webContents.send.apply(foregroundWindow.webContents, [eventName, payload]);
+        });
+    }
+
+
+}
